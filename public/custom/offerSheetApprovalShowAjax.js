22bc0928b0ccf37d3026fc5ff26349e33b08d6cc
rllynguh <atienzachristopherr@yahoo.com>
2017-08-01 02:21:45 +0800

    Updated UI for offer sheet Approval
    Incudes:
    *change from checkbox to button-modal type
    *un lang. hahah
    I love my bebi Rizza Joy Navato Isla. My one and only Inspiration. <3 <3
    <3 :* :* :*

--- public/custom/offerSheetApprovalShowAjax.js
+++ public/custom/offerSheetApprovalShowAjax.js
@@ -1,58 +1,106 @@
 $(document).ready(function()
 { 
- $.ajaxSetup(
- {
-  headers: {
-    'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+  myId="";
+  $.ajaxSetup(
+  {
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+    }
+  })
+  var table = $('#myTable').DataTable({
+    responsive: true,
+    processing: true,
+    serverSide: true,
+    ajax: {
+      url: dataurl,
+      type: "POST"
+    },
+    columns: [
+    {data: 'regi_id'},
+    {data: 'offer_id'},
+    {data: 'unit_code'},
+    {data: 'building_type'},
+    {data: 'unit_size'},
+    {data: 'unit_type'},
+    {data: 'floor'},
+    {data: 'action'}
+    ]
+  });
+
+  $(this).on('click', '.btnChoose',function(e)
+  { 
+    myId=$(this).val();
+    if($(this).val()=='header')
+    {
+      $("#modal_remarks").val($("#header_remarks").val());
+      info="Client's Remarks: " + header_remarks;
+      $('#myInfo').append(info);
+    }
+    else
+    {  
+      $("#modal_remarks").val($("#remarks"+myId).val());
+      $.get(mainUrl + '/' + $(this).val() + '/edit', function (data) 
+      {
+        console.log(data);
+        if(data.unit_type==0)
+          type='Raw';
+        else
+          type='Shell';
+        if(data.detail_remarks==null)
+          remarks="";
+        else
+          remarks=data.detail_remarks;
+        info="Offer Sheet Detail: " + data.offer_id + "<br>" +
+        "Size offered: " + data.unit_size + "<br>" +
+        "Unit Type: " + type + "<br>" +
+        "Floor: " + data.floor + "<br>" +
+        "Lessor's Remarks: " + remarks; + "<br>"
+        ;
+        $('#myInfo').append(info);
+      }); 
+    }
+    setTimeout(function(){
+      $('#modalChoose').modal('show');
+    }, 1000);
+  });
+
+
+  $(this).on('click', '#btnAccept',function(e)
+  { 
+    if(myId=="header")
+    {
+      $("#header_remarks").val($("#modal_remarks").val());
+      $("#header_is_active").val('1');
+    }
+    else
+    { 
+      $("#remarks"+myId).val($("#modal_remarks").val());
+      $("#offer"+myId).val('1');
+    }
+    $('#modalChoose').modal('hide');
+  });
+
+
+  $(this).on('click', '#btnReject',function(e)
+  { 
+   if(myId=="header")
+   {
+    $("#header_remarks").val($("#modal_remarks").val());
+    $("#header_is_active").val('2');
   }
-})
- var table = $('#myTable').DataTable({
-  responsive: true,
-  processing: true,
-  serverSide: true,
-  ajax: {
-    url: dataurl,
-    type: "POST"
-  },
-  columns: [
-  {data: 'regi_id'},
-  {data: 'offer_id'},
-  {data: 'unit_code'},
-  {data: 'building_type'},
-  {data: 'unit_size'},
-  {data: 'unit_type'},
-  {data: 'floor'},
-  {data: 'action'}
-  ]
-});
- $(this).on('change', '#checkboxReject',function(e)
- { 
-
-  if($(this).is(":checked")) 
-    $(".offer-detail").attr('disabled','disabled');
   else
-    $(".offer-detail").removeAttr('disabled','disabled');
-
-}
-);
- $(this).on('change', '.offer-detail',function(e)
- { 
-  value=false;
-       //check if all detail checkbox are checked
-       if ($('.offer-detail:checked').length == $('.offer-detail').length) {
-         value=true;
-       }
-       $('#checkboxReject').prop('checked', value);
-
-     }
-     );
- $(this).on('change', '.offer-detail',function(e)
- { 
-  myId=$(this).val();
-  if($(this).is(":checked")) 
+  { 
+    $("#remarks"+myId).val($("#modal_remarks").val())
     $("#offer"+myId).val('2');
-  else
-    $("#offer"+myId).val('1');
-}
-);
+  }
+  $('#modalChoose').modal('hide');
+});
+
+
+  $(document).on('hidden.bs.modal','#modalChoose', function () 
+  { 
+    $("#myInfo").empty();
+    $("#modal_remarks").val("");
+  });
+
 });
