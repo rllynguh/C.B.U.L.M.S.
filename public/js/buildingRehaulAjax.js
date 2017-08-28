7c46206f45671bdf114fb86e34aa666a2029c8a4
Kenneth Tan <kennethtan_02@yahoo.com>
2017-08-28 11:40:57 +0800

    fixed parsley bug

--- public/js/buildingRehaulAjax.js
+++ public/js/buildingRehaulAjax.js
@@ -1,5 +1,4 @@
-$(document).ready(function()
-{
+$(document).ready(function() {
     var page = 1;
     var current_page = 1;
     var total_page = 0;
@@ -11,74 +10,218 @@ $(document).ready(function()
     getBuildingType();
     getProvince();
     manageData();
-    setInterval( function () {
-        table.ajax.reload( null, false ); // user paging is not reset on reload
-    }, 30000 );
+    setInterval(function() {
+        table.ajax.reload(null, false); // user paging is not reset on reload
+    }, 30000);
     $.ajaxSetup({
         headers: {
-                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-            }
+            'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+        }
     });
     /* manage data list */
     function manageData() {
         var _data = [];
-        if(load === 'buildings'){
+        if (load === 'buildings') {
             _url = url;
             table = $('#myTable').DataTable({
-            responsive: true,
-            processing: true,
-            serverSide: true,
-            ajax: _url,
-            columns: [
-            {data: 'code', name: 'code', title: 'Building Code',class: 'align-center'},
-            {data: 'building_name', name: 'building_name', title: 'Building Name',class: 'align-center'},
-            {data: 'city_name', name: 'city_name', title: 'City',class: 'align-center'},
-            {data: 'is_active', name: 'is_active', title: 'Status',class: 'align-center', searchable: false},
-            {data: 'action', name: 'action', title: 'Actions',class: 'align-center', orderable: false, searchable: false}
-            ]
+                responsive: true,
+                processing: true,
+                serverSide: true,
+                ajax: _url,
+                columns: [
+                    { data: 'code', name: 'code', title: 'Building Code', class: 'align-center' },
+                    { data: 'building_name', name: 'building_name', title: 'Building Name', class: 'align-center' },
+                    { data: 'city_name', name: 'city_name', title: 'City', class: 'align-center' },
+                    { data: 'is_active', name: 'is_active', title: 'Status', class: 'align-center', searchable: false },
+                    { data: 'action', name: 'action', title: 'Actions', class: 'align-center', orderable: false, searchable: false }
+                ]
             });
-        }else if(load === 'floors'){
+        } else if (load === 'floors') {
             _url = url + "/floors/" + link[0];
             table = $('#myTable').DataTable({
-            responsive: true,
-            processing: true,
-            serverSide: true,
-            ajax: _url,
-            columns: [
-            {data: 'number', name: 'number', title: 'Floor #',class: 'align-center'},
-            {data: 'num_of_unit', name: 'num_of_unit', title: 'Number of units',class: 'align-center'},
-            {data: 'is_active', name: 'status', title: 'Status',class: 'align-center',searchable:false},
-            {data: 'action', name: 'action', title: 'Actions',class: 'align-center', orderable: false, searchable: false}
-            ]
+                responsive: true,
+                processing: true,
+                serverSide: true,
+                ajax: _url,
+                columns: [
+                    { data: 'number', name: 'number', title: 'Floor #', class: 'align-center' },
+                    { data: 'num_of_unit', name: 'num_of_unit', title: 'Number of units', class: 'align-center' },
+                    { data: 'is_active', name: 'status', title: 'Status', class: 'align-center', searchable: false },
+                    { data: 'action', name: 'action', title: 'Actions', class: 'align-center', orderable: false, searchable: false }
+                ]
             });
-            
-        }else if(load === 'units'){
+
+        } else if (load === 'units') {
             _url = url + '/units/' + link[1];
             console.log(_url);
             table = $('#myTable').DataTable({
-            responsive: true,
-            processing: true,
-            serverSide: true,
-            ajax: _url,
-            columns: [
-            {data: 'code', name: 'code', title: 'Unit ID',class: 'align-center'},
-            {data: 'type', name: 'type', title: 'Unit Type',class: 'align-center'},
-            {data: 'size', name: 'size', title: 'Area',class: 'align-center'},
-            {data: 'price', name: 'price', title: 'Rates',class: 'align-center'},
-            {data: 'is_active', name: 'status',searchable:false, title: 'Status',class: 'align-center'},
-            {data: 'action', name: 'action', orderable: false, searchable: false, title: 'Actions',class: 'align-center'}
-            ]
+                responsive: true,
+                processing: true,
+                serverSide: true,
+                ajax: _url,
+                columns: [
+                    { data: 'code', name: 'code', title: 'Unit ID', class: 'align-center' },
+                    { data: 'type', name: 'type', title: 'Unit Type', class: 'align-center' },
+                    { data: 'size', name: 'size', title: 'Area', class: 'align-center' },
+                    { data: 'price', name: 'price', title: 'Rates', class: 'align-center' },
+                    { data: 'is_active', name: 'status', searchable: false, title: 'Status', class: 'align-center' },
+                    { data: 'action', name: 'action', orderable: false, searchable: false, title: 'Actions', class: 'align-center' }
+                ]
             });
 
             //$("thead").html('<th class="align-center">Unit ID</th><th class="align-center">Unit Type</th><th class="align-center">Area</th><th class="align-center">Rate</th><th class="align-center">Status</th><th class="align-center">Action</th>'); 
         }
-        
-       
+
+
     };
 
-    /* Get Page Data
+
+    /* Create new Item */
+    $("#btnBuildingSave").click(function(e) {
+        if ($('#buildingCreateForm').parsley().isValid()) {
+            console.log('c');
+            $("#btnBuildingSave").attr('disabled', 'disabled');
+            setTimeout(function() {
+                $("#btnBuildingSave").removeAttr('disabled');
+            }, 1000);
+            e.preventDefault();
+            var formData = $("#buildingCreateForm").serialize();
+            var form_action = $("#buildingCreateModal").find("form").attr("action");
+            $.ajax({
+                type: 'POST',
+                url: form_action,
+                data: formData,
+            }).done(function(data) {
+                $(".modal").modal('hide');
+                toastr.success('Item Created Successfully.', 'Success Alert', { timeOut: 5000 });
+            });
+        }
+
+
+    });
+    /* Show floors */
+    $("body").on("click", ".show-floors", function() {
+        load = "floors";
+        link[0] = $(this).attr("data-id")
+        console.log(link[0] + "," + load);
+        table.destroy();
+        $('#myTable').empty();
+        manageData();
+    });
+    /* Show Units */
+    $("body").on("click", ".show-units", function() {
+        load = "units";
+        link[1] = $(this).attr("data-id")
+        console.log(link[1] + "," + load);
+        table.destroy();
+        $('#myTable').empty();
+        manageData();
+    });
+    /* Remove Item */
+    $("body").on("click", ".remove-item", function() {
+        var id = $(this).parent("td").data('id');
+        var c_obj = $(this).parents("tr");
+        $.ajax({
+            dataType: 'json',
+            type: 'delete',
+            url: url + '/' + id,
+        }).done(function(data) {
+            c_obj.remove();
+            toastr.success('Item Deleted Successfully.', 'Success Alert', { timeOut: 5000 });
+            getPageData();
+        });
+    });
+
+    /* Edit Item */
+    $("body").on("click", ".edit-item", function() {
+        var id = $(this).parent("td").data('id');
+        var title = $(this).parent("td").prev("td").prev("td").text();
+        var description = $(this).parent("td").prev("td").text();
+        $("#edit-item").find("input[name='title']").val(title);
+        $("#edit-item").find("textarea[name='description']").val(description);
+        $("#edit-item").find("form").attr("action", url + '/' + id);
+    });
+
+    /* Updated new Item */
+    $(".crud-submit-edit").click(function(e) {
+        e.preventDefault();
+        var form_action = $("#edit-item").find("form").attr("action");
+        var title = $("#edit-item").find("input[name='title']").val();
+        var description = $("#edit-item").find("textarea[name='description']").val();
+        $.ajax({
+            dataType: 'json',
+            type: 'PUT',
+            url: form_action,
+            data: { title: title, description: description }
+        }).done(function(data) {
+            getPageData();
+            $(".modal").modal('hide');
+            toastr.success('Item Updated Successfully.', 'Success Alert', { timeOut: 5000 });
+        });
+    });
+});
+
+function getBuildingType() {
+    $.get(urlbtype, function(data) {
+        $('#building_type').children('option').remove();
+        $.each(data, function(index, value) {
+            $('#building_type').append($('<option>', { value: value.id, text: value.description }));
+        });
+    });
+}
+
+function getProvince() {
+    $.get(urlprov, function(data) {
+        $('#building_province').children('option').remove();
+        $.each(data, function(index, value) {
+            $('#building_province').append($('<option>', { value: value.id, text: value.description }));
+        });
+        getCity();
+    });
+}
+
+
+//for querying list of city
+function getCity() {
+    $.get('/custom/getCity/' + $("#building_province").val(), function(data) {
+        $('#building_city').children('option').remove();
+        $.each(data, function(index, value) {
+            $('#building_city').append($('<option>', { value: value.id, text: value.description }));
+        });
+    });
+}
+
+//for querying city when user selects province
+$("#building_province").change(function(data) {
+    getCity();
+});
+/*
+ $.ajax({
+            dataType: 'json',
+            url: _url,
+            data: {page:page}
+        }).done(function(data){
+
+            total_page = data.last_page;
+            current_page = data.current_page;
+
+            $('#pagination').twbsPagination({
+                totalPages: total_page,
+                visiblePages: current_page,
+                onPageClick: function (event, pageL) {
+                    page = pageL;
+                    if(is_ajax_fire != 0){
+                      getPageData();
+                    }
+                }
+            });
+
+            manageRow(data.data);
+            is_ajax_fire = 1;
+        });
+             Get Page Data
     Old Code, might need it again if shit doesn't work
-    */
+    
     function getPageData() {
         $.ajax({
             dataType: 'json',
@@ -89,9 +232,8 @@ $(document).ready(function()
         });
     }
 
-    /* Add new Item table row
+    Add new Item table row
     Old Code, might need it again if shit doesn't work
-     */
     function manageRow(data) {
         var rows = '';
         $.each( data, function( key, value ) {
@@ -152,151 +294,4 @@ $(document).ready(function()
         });
         $("tbody").html(rows);
     };
-    /* Create new Item */
-    $("#btnBuildingSave").click(function(e){
-        $("#btnBuildingSave").attr('disabled','disabled');
-          setTimeout(function(){
-            $("#btnBuildingSave").removeAttr('disabled');
-          }, 1000);
-        e.preventDefault();
-        var formData = $("#buildingCreateForm").serialize(); 
-        var form_action = $("#buildingCreateModal").find("form").attr("action");
-        $.ajax({
-            type:'POST',
-            url: form_action,
-            data:formData,
-        }).done(function(data){
-            getPageData();
-            $(".modal").modal('hide');
-            toastr.success('Item Created Successfully.', 'Success Alert', {timeOut: 5000});
-        });
-
-    });
-    /* Show floors */
-    $("body").on("click",".show-floors",function(){
-        load = "floors";
-        link[0] = $(this).attr("data-id")
-        console.log(link[0] + "," +load );
-        table.destroy();
-        $('#myTable').empty();
-        manageData();
-    });
-      /* Show Units */
-    $("body").on("click",".show-units",function(){
-        load = "units";
-        link[1] = $(this).attr("data-id")
-        console.log(link[1] + "," +load );
-        table.destroy();
-        $('#myTable').empty();
-        manageData();
-    });
-    /* Remove Item */
-    $("body").on("click",".remove-item",function(){
-        var id = $(this).parent("td").data('id');
-        var c_obj = $(this).parents("tr");
-        $.ajax({
-            dataType: 'json',
-            type:'delete',
-            url: url + '/' + id,
-        }).done(function(data){
-            c_obj.remove();
-            toastr.success('Item Deleted Successfully.', 'Success Alert', {timeOut: 5000});
-            getPageData();
-        });
-    });
-
-    /* Edit Item */
-    $("body").on("click",".edit-item",function(){
-        var id = $(this).parent("td").data('id');
-        var title = $(this).parent("td").prev("td").prev("td").text();
-        var description = $(this).parent("td").prev("td").text();
-        $("#edit-item").find("input[name='title']").val(title);
-        $("#edit-item").find("textarea[name='description']").val(description);
-        $("#edit-item").find("form").attr("action",url + '/' + id);
-    });
-
-    /* Updated new Item */
-    $(".crud-submit-edit").click(function(e){
-        e.preventDefault();
-        var form_action = $("#edit-item").find("form").attr("action");
-        var title = $("#edit-item").find("input[name='title']").val();
-        var description = $("#edit-item").find("textarea[name='description']").val();
-        $.ajax({
-            dataType: 'json',
-            type:'PUT',
-            url: form_action,
-            data:{title:title, description:description}
-        }).done(function(data){
-            getPageData();
-            $(".modal").modal('hide');
-            toastr.success('Item Updated Successfully.', 'Success Alert', {timeOut: 5000});
-        });
-    });
-
-});
-function getBuildingType()
-{
- $.get(urlbtype, function (data) {
-  $('#building_type').children('option').remove();
-  $.each(data,function(index,value)
-  {
-    $('#building_type').append($('<option>', {value:value.id, text:value.description}));
-  });
-});
-}
-
-
-function getProvince()
-{
-  $.get(urlprov, function (data) {
-    $('#building_province').children('option').remove();
-    $.each(data,function(index,value)
-    {
-      $('#building_province').append($('<option>', {value:value.id, text:value.description}));
-    });
-    getCity();
-  });
-}
-
-
-//for querying list of city
-function getCity()
-{
-  $.get('/custom/getCity/' + $("#building_province").val(), function (data) {
-    $('#building_city').children('option').remove();
-    $.each(data,function(index,value)
-    {
-      $('#building_city').append($('<option>', {value:value.id, text:value.description}));
-    });
-  });
-}
-
-//for querying city when user selects province
-$("#building_province").change(function(data){
-  getCity();
-});
-/*
- $.ajax({
-            dataType: 'json',
-            url: _url,
-            data: {page:page}
-        }).done(function(data){
-
-            total_page = data.last_page;
-            current_page = data.current_page;
-
-            $('#pagination').twbsPagination({
-                totalPages: total_page,
-                visiblePages: current_page,
-                onPageClick: function (event, pageL) {
-                    page = pageL;
-                    if(is_ajax_fire != 0){
-                      getPageData();
-                    }
-                }
-            });
-
-            manageRow(data.data);
-            is_ajax_fire = 1;
-        });
         */
