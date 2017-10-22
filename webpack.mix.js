4a71b440f4112c9195e9ea074eb7a8c9a084583c
Kenneth Tan <kennethtan_02@yahoo.com>
2017-10-22 10:08:28 +0800

    shit

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,4 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/tenant.scss','public/css/tenant')
-   .js('resources/assets/js/tenant.js','public/js/tenant');
+   .sass('resources/assets/sass/tenant.scss','public/css/tenant');
